3bb52081d22a4bfad5709f49f6ce31930325ddca
plur9 <gregor@plur.si>
2026-04-22 16:27:42 +0200

    chore: align all packages to 0.9.2

--- packages/claw/src/context-engine.ts
+++ packages/claw/src/context-engine.ts
@@ -50,7 +50,7 @@ export class PlurContextEngine implements ContextEngine {
   readonly info: ContextEngineInfo = {
     id: 'plur-claw',
     name: 'PLUR Memory Engine',
-    version: '0.9.0',
+    version: '0.9.2',
     ownsCompaction: false,
   }
 
