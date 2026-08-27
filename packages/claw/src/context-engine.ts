67b3aeb2c9889ec1326a994edb2f192dedf66626
plur9 <gregor@plur.si>
2026-08-28 01:17:34 +0200

    chore: release v0.19.0

--- packages/claw/src/context-engine.ts
+++ packages/claw/src/context-engine.ts
@@ -59,7 +59,7 @@ export class PlurContextEngine implements ContextEngine {
   readonly info: ContextEngineInfo = {
     id: 'plur-claw',
     name: 'PLUR Memory Engine',
-    version: '0.17.0',
+    version: '0.17.1',
     ownsCompaction: false,
   }
 
